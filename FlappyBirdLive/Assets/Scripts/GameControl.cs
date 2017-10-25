4ea66ca2232682567f8e74a3db6b840c909b424e
Ole Helland <ole.helland@gmail.com>
2017-10-25 22:15:20 +0200

    Add project

---
+++ FlappyBirdLive/Assets/Scripts/GameControl.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameControl : MonoBehaviour {
+
+	// Deklareringer
+	public static GameControl instance;
+
+	public GameObject gameOverText;
+	public bool gameOver = false;
+
+	public float scrollSpeed = -1.5f;
+
+	public Text scoreText; 
+	public int score = 0;
+
+	public Text highestScoreText;
+	public int highestScore;
+
+	public DataControl dataControl;
+
+	// Awake starter før void Start.
+	void Awake () 
+	{
+		if (instance == null) 
+		{
+			instance = this;
+		} else if (instance != this)
+		{
+			Destroy (gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (gameOver == true && Input.GetMouseButtonDown (0)) 
+		{
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+		}
+	}
+
+	// Æsj
+	public void BirdScored () 
+	{
+		if (gameOver) 
+		{
+			return;
+		}
+		SoundManagerScript.PlaySound ("pointGet");
+		score++;
+		scoreText.text = "Score: " + score.ToString();
+	}
+
+	// Trigger Game Over staten
+	public void BirdDied() 
+	{
+		gameOverText.SetActive (true);
+		gameOver = true;
+		dataControl.SubmitNewPlayerScore (score);
+		highestScoreText.text = "High score: " + dataControl.GetHighestPlayerScore ().ToString ();
+	}
+}
---
+++ FlappyBirdLive/Assets/Scripts/ScrollingObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingObject : MonoBehaviour {
+
+	private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+		// Sørger for at bakgrunnen gjentar seg
+		rb2d = GetComponent<Rigidbody2D> ();
+		rb2d.velocity = new Vector2 (GameControl.instance.scrollSpeed, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Stopper scrollingen etter game over
+		if (GameControl.instance.gameOver == true) {
+			rb2d.velocity = Vector2.zero;
+		}
+	}
+}
