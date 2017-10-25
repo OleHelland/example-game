6c94a0c7db4b45947741db0668733b7084fdc4a1
Ole Helland <ole.helland@gmail.com>
2017-10-25 23:47:45 +0200

    Revert "Revert "Add project""
    This reverts commit af45ed7a574629ee1c54c293061c40b5ce2a6ad1.

---
+++ FlappyBirdLive/Assets/Scripts/SoundManagerScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManagerScript : MonoBehaviour {
+
+	public static AudioClip birdHitSound, birdFlapSound, pointGetSound;
+	static AudioSource audioSrc;
+
+	// Use this for initialization
+	void Start () {
+
+		birdHitSound = Resources.Load<AudioClip> ("birdHit");
+		birdFlapSound = Resources.Load<AudioClip> ("birdFlap");
+		pointGetSound = Resources.Load<AudioClip> ("pointGet");
+
+		audioSrc = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public static void PlaySound (string clip)
+	{
+		switch (clip) {
+		case "birdHit":
+			audioSrc.PlayOneShot (birdHitSound);
+			audioSrc.pitch = (Random.Range(.9f, 1.2f));
+			break;
+		case "birdFlap":
+			audioSrc.PlayOneShot (birdFlapSound);
+			audioSrc.pitch = (Random.Range(.9f, 1.2f));
+			break;
+		case "pointGet":
+			audioSrc.PlayOneShot (pointGetSound);
+			break;
+		}
+	}
+}
